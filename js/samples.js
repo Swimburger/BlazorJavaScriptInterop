2b4a559e5f6fefc2f05a0e41e175f13af823715e
Swimburger <nielsswimberghe@gmail.com>
2020-10-31 22:27:05 +0000

    Deploying to gh-pages from  @ 818108137e1c84de1c9aa78c1ac73a923d297a4e 🚀

---
+++ js/samples.js
@@ -0,0 +1,60 @@
+// .NET to JS samples
+window.dotNetToJsSamples = {
+    setText: function(node, text)
+    {
+        node.textContent = text;
+    },
+    printDotNetObject: function(node, objectFromDotNet)
+    {
+        node.textContent = JSON.stringify(objectFromDotNet);
+    },
+    getValue: function(node)
+    {
+        return node.value;
+    },
+    getWeatherData: function(){
+        return fetch('/sample-data/weather.json')
+            .then(response => response.json());
+    }
+};
+
+// JS to .NET samples
+window.jsToDotNetSamples = {
+    dotNetReference: null,
+    setDotNetReference: function(dotNetReference)
+    {
+        this.dotNetReference = dotNetReference;
+    },
+    printPersonFromDotNet: function(){
+        var person = this.dotNetReference.invokeMethod("GetPerson");
+        document.getElementById('personCodeBlock').textContent = JSON.stringify(person);
+    },
+    getWeatherDataFromDotNet: function(node){
+        this.dotNetReference.invokeMethodAsync('GetWeatherAsync')
+            .then(data => {
+                document.querySelector('code').textContent = JSON.stringify(data);
+            });
+    }
+};
+
+var blazorCarousel = {
+    instances: {},
+    init: function (carouselId, dotNetReference, carouselNode) {
+        var carousel = {
+            dotNetReference: dotNetReference,
+            $carouselNode: $(carouselNode)
+        };
+        carousel.$carouselNode.carousel();
+        carousel.$carouselNode.on('slide.bs.carousel', function (event) {
+            dotNetReference.invokeMethod("OnSlide", event.direction, event.from, event.to);
+        });
+        carousel.$carouselNode.on('slid.bs.carousel', function (event) {
+            dotNetReference.invokeMethod("OnSlid", event.direction, event.from, event.to);
+        });
+        this.instances[carouselId] = carousel;
+    },
+    dispose: function (carouselId) {
+        this.instances[carouselId].$carouselNode.carousel('dispose');
+        delete this.instances[carouselId];
+    }
+};
